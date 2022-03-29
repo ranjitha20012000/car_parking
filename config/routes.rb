d7331849317ba6137789c5515b044169fa1f9934
Mogana070199 <moganamogzofficial@gmail.com>
2022-03-29 10:15:14 +0530

    merged _file _changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'dashboard#home'
-  root "home#index"
+  #root "home#index"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_25_104758) do
     t.datetime "updated_at", null: false
   end
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_28_041106) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_28_041106) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
